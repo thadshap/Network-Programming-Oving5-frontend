65f173f6be6f656510d344ff8664073cf17266ad
Thadshajini Paramsothy <thadshap@stud.ntnu.no>
2022-03-03 15:08:53 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+
   </div>
   <router-view />
 </template>
@@ -28,3 +27,6 @@
   color: #42b983;
 }
 </style>
+<script>
+
+</script>
---
+++ src/views/CodeEditor.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="main">
+    <h1>Code Editor</h1>
+    <textarea id="code"></textarea>
+    <button id="run">Run</button>
+    <div id="result"></div>
+  </div>
+</template>
+
+
+
+<style scoped>
+h1{
+  text-align: center;
+}
+textarea{
+  width: 600px;
+  height: 500px;
+  margin: 20px 0 0 32%;
+}
+#run{
+  margin-left: 10px;
+}
+#result{
+  display: flex;
+  width: 602px;
+  height: 50px;
+  border:1px solid black;
+  margin: 20px 0 0 32%;
+}
+</style>
